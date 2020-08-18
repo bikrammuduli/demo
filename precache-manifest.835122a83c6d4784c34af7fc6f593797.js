b2453735d16532f2e776780020a3dd6c29e23dbe
digientropy <bikrammuduli1@gmail.com>
2020-08-18 19:05:18 +0530

    Updates

---
+++ precache-manifest.835122a83c6d4784c34af7fc6f593797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120543929e2575309630360da63ac8ea",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "bf2e0eda0f02bfad7bd7",
+    "url": "/demo/static/css/main.7e02edc9.chunk.css"
+  },
+  {
+    "revision": "9ecad23ab8135fdd27a9",
+    "url": "/demo/static/js/2.26d16d4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo/static/js/2.26d16d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2e0eda0f02bfad7bd7",
+    "url": "/demo/static/js/main.4fc4672d.chunk.js"
+  },
+  {
+    "revision": "b39b67791598b779560b",
+    "url": "/demo/static/js/runtime-main.a819b733.js"
+  }
+]);
